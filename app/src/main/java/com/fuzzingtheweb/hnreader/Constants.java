d13c00b721e6008f591d24e179de8191948e78c1
Javi Manzano <javi.manzano.oller@gmail.com>
2014-11-20 00:01:54 +0000

    New constants

--- app/src/main/java/com/fuzzingtheweb/hnreader/Constants.java
+++ app/src/main/java/com/fuzzingtheweb/hnreader/Constants.java
@@ -9,4 +9,7 @@ public final class Constants {
     public static final String KEY_TITLE = "title";
     public static final String KEY_TYPE = "type";
     public static final String KEY_URL = "url";
+    public static final String KEY_TEXT = "text";
+    public static final String KEY_TOP_STORIES_URL = "https://hacker-news.firebaseio.com/v0/topstories";
+    public static final String KEY_ITEM_URL = "https://hacker-news.firebaseio.com/v0/item/";
 }
